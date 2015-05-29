cd09011be7216613b16df86e88ee17c11284459a
vishu87 <vishu.iitd@gmail.com>
2015-05-29 16:28:18 +0530

    sidebar href links

--- site/app/views/frontend/sidebar.blade.php
+++ site/app/views/frontend/sidebar.blade.php
@@ -2,7 +2,7 @@
 	<?php $count = 0; ?>
 	@foreach($sidebar as $item)
 		@if($item->type == 1)
-			@if($item->page_title) {{$item->page_title}} @endif
+			@if($item->page_title) <a href="{{URL::to('/'.$item->page_slug)}}">{{$item->page_title}}</a> @endif
 		@endif
 		@if($item->type == 2)
 			<div class="sidebar-title" @if($count == 0) style="margin-top:0" @endif>
