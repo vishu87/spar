a15636a2836b97bf92a7804c6a6e2ef0e19a3c5c
Vashistha Aggarwal <vishu.iitd@gmail.com>
2015-10-11 17:18:09 +0530

    brands front end change 1

--- site/app/views/frontend/brands.blade.php
+++ site/app/views/frontend/brands.blade.php
@@ -1,13 +1,13 @@
-
 <div class="row">	
 	@foreach($brands as $brand)
-		<div class="col-md-4 deal-box-contain">
+		<div class="col-md-3 deal-box-contain">
 			<div style="position:relative">
 				{{ HTML::image('images/'.$brand->brand_image,'',array("class"=>"recipe-img-widthx border-radius-60")) }}
 				<div class="brand-name border-radius-50" align="center">
 					{{$brand->brand_name}}
-				</div>								   
+				</div>							   
 			</div>
+			<p>{{$brand->description}}</p>
 		</div>
 	@endforeach
 </div>
